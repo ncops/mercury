9c5c0ed7c19d2320c7b69eac8f6255876fc6948a
Rubatharisan Thirumathyam <ruth@netcompany.com>
2019-05-01 02:16:38 +0200

    Making test elements

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const expect = require('chai').expect;
 const dom = require('cheerio');
+const xray = require(__dirname + '/tests/xray');
 
-const logo = "/resources/erhvervsstyrelsen_logo.png";
+let failedTests = {};
 
-describe('#ID: OK-7 - Ændre logo på skaleringsportal', function () {
-    it('Log file must reside in resources', function () {
+describe('OK-7', function () {
+    const logo = "/resources/erhvervsstyrelsen_logo.png";
+
+    it('Logo file must reside in resources', function () {
         // Check if logo resides in public/resources/
         if (!fs.existsSync(__dirname + '/public' + logo)){
             throw new Error("The logo file does not exist in the resources folder");
@@ -24,5 +27,18 @@ describe('#ID: OK-7 - Ændre logo på skaleringsportal', function () {
         );
 
     });
+
+
+    before(() => {
+        const userstory = this.title;
+        failedTests[userstory] = [];
+        afterEach(function() {
+            if(this.currentTest.state === "failed"){
+                failedTests[userstory].push(this.currentTest);
+                process.env['BUILD_URL'] = 'https://google.dk/';
+            }
+        });
+    });
 });
+
 
